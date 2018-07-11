76ae41dfeb96c178ae034cafcfc18d1b5f759c50
Tyler Westlie <tyler.westlie@gmail.com>
2018-07-11 10:17:50 -0600

    refactored the station service method, added vcr config

--- app/services/station_service.rb
+++ app/services/station_service.rb
@@ -4,16 +4,19 @@ class StationService
   end
 
   def search_result
-    search.map do |s|
+    data[:fuel_stations].map do |s|
       Station.new(s)
     end
   end
 
-  def search
-    data = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{@zip}&fuel_type=E85,ELEC&radius=10&limit=10&api_key=#{ENV['NREL_API_KEY']}&format=JSON")
+  private
+    attr_reader :zip
 
-    parsed = JSON.parse(data.body, symbolize_names: true)
+    def response
+      Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{@zip}&fuel_type=E85,ELEC&radius=10&limit=10&api_key=#{ENV['NREL_API_KEY']}&format=JSON")
+    end
 
-    parsed[:fuel_stations]
-  end
+    def data
+      JSON.parse(response.body, symbolize_names: true)
+    end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,15 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
+end
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
