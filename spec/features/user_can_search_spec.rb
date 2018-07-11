b952a3c347feb5808a83e3c74b91936bc5d240bd
Tyler Westlie <tyler.westlie@gmail.com>
2018-07-11 09:29:46 -0600

    added code to the views

--- spec/features/user_can_search_spec.rb
+++ spec/features/user_can_search_spec.rb
@@ -4,11 +4,24 @@ describe 'user can search for alt fuels' do
   it 'can use a zip code to find attributes' do
     # As a user
     # When I visit "/"
+    visit '/'
     # And I fill in the search form with 80203 (Note: Use the existing search form)
+    # save_and_open_page
+    fill_in :q, with: 80203
     # And I click "Locate"
+    click_on 'Locate'
     # Then I should be on page "/search"
+    expect(current_path).to eq(search_path)
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     # And the stations should be limited to Electric and Propane
+
+    within('.station') do
+      expect(page).to have_css('.name')
+      expect(page).to have_css('.address')
+      expect(page).to have_css('.fuel_type')
+      expect(page).to have_css('.distance')
+      expect(page).to have_css('.access_time')
+    end
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
   end
 end
